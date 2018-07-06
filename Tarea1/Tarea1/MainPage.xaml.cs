b4faaeb566e4cab42acb3113dd7750cb5c85935f
Moises Martinez <moisesmartinezliranzo@gmail.com>
2018-07-06 17:48:17 -0400

    Add project files.

---
+++ Tarea1/Tarea1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1
+{
+	public partial class MainPage : ContentPage
+	{
+        int counter = 0;
+
+        public MainPage()
+		{
+			InitializeComponent();
+            label.Text = "Hola, haz hecho " + counter + " click";
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            counter++;
+            label.Text = "Hola, haz hecho " + counter + " click";
+        } 
+    }
+}
